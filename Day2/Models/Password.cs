492136e2496c622a816a1235341d261b945d1863
Aimee Simmons <aimee.simmons@bglgroup.co.uk>
2020-12-02 10:37:26 +0000

    Implemented Day 2 solution.

---
+++ Day2/Models/Password.cs
@@ -0,0 +1,25 @@
+﻿namespace Day2.Models
+{
+    public class Password
+    {
+        public Password(string passwordString)
+        {
+            var splitPasswordString = passwordString.Split(" ");
+            var splitPasswordPolicy = splitPasswordString[0].Split("-");
+
+            PasswordPolicy = new PasswordPolicy
+            {
+                MinAmount = int.Parse(splitPasswordPolicy[0]),
+                MaxAmount = int.Parse(splitPasswordPolicy[1])
+            };
+            Character = char.Parse(splitPasswordString[1].Trim(':'));
+            PasswordText = splitPasswordString[2];
+        }
+
+        public PasswordPolicy PasswordPolicy { get; set; }
+
+        public char Character { get; set; }
+
+        public string PasswordText { get; set; }
+    }
+}
---
+++ Day2/Models/PasswordPolicy.cs
@@ -0,0 +1,9 @@
+﻿namespace Day2.Models
+{
+    public class PasswordPolicy
+    {
+        public int MinAmount { get; set; }
+
+        public int MaxAmount { get; set; }
+    }
+}
